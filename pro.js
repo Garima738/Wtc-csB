e202f966cee2c262bf1d728473680f511452249d
garima <garima@garimas-MacBook-Air.local>
2026-02-04 11:31:28 +0530

    Initital commit

---
+++ pro.js
@@ -0,0 +1,63 @@
+// const name=prompt("Enter your name");
+// const age = prompt("Enter your age");
+// const info=`My name is ${name} and age is ${age}`;
+// console.log(info);
+// document.write(info); //to show on brower after entering data.
+// //prompt does not work on terminal.
+
+
+//-----------calculator------------
+// const cal=()=>({
+//  add :(a, b) => a + b,
+//  sub :(a, b) => a - b,
+//  multiply :(a, b) => a * b,
+//  divide : (a, b) => (b===0 ?"Error ": a/b),});
+//  console.log(cal().add(11,5));
+//  console.log(cal().sub(11,5));
+//  console.log(cal().multiply(11,5));
+//  console.log(cal().divide(11,5));
+
+
+//----------using switch-------
+// const calculator = (a, b, op) => {
+
+//     let result;
+
+//     switch (op) {
+//         case "+":
+//             result = a + b;
+//             break;
+
+//         case "-":
+//             result = a - b;
+//             break;
+
+//         case "*":
+//             result = a * b;
+//             break;
+
+//         case "/":
+//             if (b === 0) {
+//                 return "Cannot divide by zero";
+//             }
+//             result = a / b;
+//             break;
+
+//         default:
+//             return "Invalid operator";
+//     }
+
+//     return result;
+// };
+
+
+// console.log(calculator(10, 5, "+"));
+// console.log(calculator(10, 5, "-"));
+// console.log(calculator(10, 5, "*"));
+// console.log(calculator(10, 5, "/"));
+
+   
+
+
+
+
